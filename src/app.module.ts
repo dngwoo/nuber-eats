b43df335465222b6f9dd32bef2995b3c9093b21a
dngwoo <akfmzk84@naver.com>
2020-12-30 19:40:36 +0900

    #2.6 Validating ConfigService (05:44), 환경변수 오류 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
@@ -9,7 +10,15 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? 'env.dev' : 'env.test',
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -17,7 +26,7 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      database: process.env.DB_NAME,
       synchronize: true, // 데이터베이스를 나의 모듈의 현재상태로 마이그레이션
       logging: true, // 데이터베이스에서 무슨 일이 일어나는지 콘솔에 표시
     }),
